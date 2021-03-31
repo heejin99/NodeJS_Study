03b0166e2e26073d0fbce9373f68ca497a03039d
heejin99 <wkdgmlwls30@naver.com>
2021-03-31 16:22:52 +0900

    Revert "NodeJS 교과서 app.js 미들웨어 추가"
    This reverts commit 5e68053442fdf9e4c620ee2786d7f225e0c75ef2.

--- NodeJS_Book_Practice/Chapter6/app.js
+++ NodeJS_Book_Practice/Chapter6/app.js
@@ -2,27 +2,13 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
 app.set('port', process.env.PORT || 3000); // process.env.port가 없다면 기본값으로 3000번 포트
-// 미들웨어 연결
-app.use((req, res, next) => {
-    console.log('모든 요청에 다 실행됩니다.');
-    next();
-});
 
-app.get('/', (req, res, next) => {
-    console.log('GET / 요청에서만 실행됩니다.');
-    next();    
-}, (req, res) => {
-    throw new Error('에러는 에러 처리 미들 웨어로 갑니다.');
-});
-
-app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500).send(err.message);
+app.get('/', (req, res) => {
+    // res.send('Hello, Express'); // GET 요청시 응답으로 hello, express 전송
+    res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기 중');
 }) ;
-// dotenv를 제외한 다른 패키지는 미들웨어 (dotenv: process.env 관리)
